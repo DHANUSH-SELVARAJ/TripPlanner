ea9117815240b3113fa08cdddd659c711af40e1b
DHANUSH-SELVARAJ <mr.dhanushselvaraj@gmail.com>
2025-08-02 12:13:37 +0530

    fixed the loginpage issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
-import LoginPage from './pages/LoginPage';
 import Register from './pages/Register'; // Import your Register page
 import PrivateRoute from './Route/PrivateRoute';
 import MapPage from './pages/MapPage';
+import LoginPage from './pages/LoginPage';
 
 export default function App() {
   return (
     <Router>
       <Routes>
         {/* Public routes */}
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage/>} />
         <Route path="/register" element={<Register />} />
 
         {/* Redirect root to login */}
